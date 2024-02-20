c26dffa5a2a58123e383314204967ab2af5a9b70
TamlynSpringer <tamlynspringer@gmail.com>
2024-02-20 22:41:44 +0100

    Mobile sidebar using dropdown menu

---
+++ app/(client)/_components/sidebar/MobileSidebar.tsx
@@ -0,0 +1,116 @@
+'use client'
+import {
+  ChevronsLeft,
+  ChevronsRight,
+  LogOut,
+  Menu,
+} from "lucide-react"
+import { useEffect, useRef, useState } from "react";
+import { useRouter } from "next/navigation";
+import Link from "next/link";
+import { signOut } from "next-auth/react";
+import UserCard from "./UserCard";
+import SidebarItemRenderer from "./SidebarItems";
+import { SidebarCompProps } from "@/lib/interfaces";
+import { CldImage } from 'next-cloudinary';import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/app/components/ui/dropdown-menu";
+import IconRenderer from "./Icons";
+
+
+const MobileSidebar: React.FC<SidebarCompProps> = ({ userRoles, session }) => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const user = session?.user;
+  const router = useRouter();
+  const logoUrl = `https://res.cloudinary.com/dsbvy1t2i/image/upload/v1707912829/${user.userDomain}.png`;
+
+  const handleToggleMenu = () => {
+    setIsMenuOpen((prevIsOpen) => !prevIsOpen);
+  };
+
+  const handleSignOut = async () => {
+    await signOut({ callbackUrl: '/' });
+    router.replace('/');
+  };
+
+    const sidebarItems = [
+      { role: 'EMPLOYEE', label: 'Profile', href: '/dashboard/employee-profile' },
+      { role: 'ADMIN', label: 'Admin dashboard', href: '/dashboard/admin' },
+      { role: 'EMPLOYEE', label: 'Recruitment', href: '#' },
+      { role: 'EMPLOYEE', label: 'Inbox', href: '#' },
+      { role: 'ADMIN', label: 'Users', href: '#' },
+      { role: 'EMPLOYEE', label: 'Preferences', href: '#' },
+      { role: 'EMPLOYEE', label: 'Settings', href: '#' },
+    ];
+  
+    let uniqueRoles: string[] = [];
+    let filteredSidebarItems;
+    const [selectedItem, setSelectedItem] = useState<string | null>(null);
+  
+    // Get all unique roles of the user
+    if (userRoles) {
+      uniqueRoles = userRoles?.filter((role: string, index: number, roles: string[]) => roles.indexOf(role) === index);
+    }
+  
+    // Filter sidebar items based on the user's roles
+    if (sidebarItems) {
+      filteredSidebarItems = sidebarItems.filter((item) => uniqueRoles.includes(item.role));
+    }
+  
+    const getIconComponent = (iconName: string) => {
+      return <IconRenderer iconName={iconName} />;
+    };
+
+    function capitalizeFirstLetter(str: string): string {
+      return str.charAt(0).toUpperCase() + str.slice(1);
+    }
+
+
+  return (
+    <DropdownMenu>
+      <div className="flex items-center justify-between text-foreground">
+        <DropdownMenuTrigger
+          className="p-2 hover:scale-125"
+          onClick={handleToggleMenu}
+        >
+          <Menu size={24} />
+        </DropdownMenuTrigger>
+        {isMenuOpen && (
+          <DropdownMenuContent>
+            <DropdownMenuLabel>
+              <Link href='/'>
+                <CldImage alt={`${user?.userDomain} logo`} src={logoUrl} width={40} height={40} className="object-cover ml-5 my-2" />
+              </Link>
+              <span className="text-lg font-bold ml-4">{capitalizeFirstLetter(user?.userDomain)}</span>
+            </DropdownMenuLabel>
+            {filteredSidebarItems?.map((item, index) => (
+              <DropdownMenuItem key={index}>
+                <Link href={item.href} className="flex gap-4">
+                  {getIconComponent(item.label)}
+                  {item.label}
+                </Link>
+              </DropdownMenuItem>
+            ))}
+            <DropdownMenuItem>
+              <UserCard session={session} isMenuOpen={isMenuOpen} />
+            </DropdownMenuItem>
+            <DropdownMenuItem className="my-4">
+              <button className="flex items-center cursor-pointer ml-4" onClick={handleSignOut}>
+                <LogOut className="h-5 w-5 hover:scale-125" />
+                <span className="ml-2">Sign out</span>
+              </button>
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        )}
+      </div>
+    </DropdownMenu>
+  );
+};
+
+export default MobileSidebar;
+
--- app/(client)/sidebar/page.tsx
+++ app/(client)/sidebar/page.tsx
@@ -1,14 +1,34 @@
+'use client'
+import { useEffect, useState } from "react";
+import MobileSidebar from "../_components/sidebar/MobileSidebar";
 import SidebarComp from "../_components/sidebar/Sidebar";
 import { SidebarProps } from "@/lib/interfaces";
 
-const Sidebar: React.FC<SidebarProps> = async ({ session }) => {
+const Sidebar: React.FC<SidebarProps> = ({ session }) => {
   const userRoles = session?.role || [];
   console.log(session, userRoles, 'user roles & session in side bar page');
 
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth < 500); 
+    };
+    handleResize();
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   return (
-    <div className="h-dvh">
-      <SidebarComp userRoles={userRoles} session={session} />
-    </div>
+    <aside className="h-dvh">
+     {isMobile ? (
+        <MobileSidebar userRoles={userRoles} session={session} />
+      ) : (
+        <SidebarComp userRoles={userRoles} session={session} />
+      )}
+    </aside>
   );
 };
 
